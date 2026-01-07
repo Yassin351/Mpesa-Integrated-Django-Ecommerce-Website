bddae349008fdf010121b9eaef4a96a3db1fcda3
Yassin351 <soleyassin@gmail.com>
2026-01-06 20:09:49 -0800

    Fix Python version and force binary Pillow installation

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ set -o errexit
 # Upgrade build tools first
 pip install --upgrade pip setuptools wheel
 
-# Install requirements with binary wheels when possible
+# Install Pillow with binary wheel to avoid compilation
+pip install --only-binary=:all: Pillow==10.3.0
+
+# Install remaining requirements
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
