b3dac69ae8431e210ef0086bec703b6e4f4873dc
Yassin351 <soleyassin@gmail.com>
2026-01-06 20:04:40 -0800

    Fix Pillow build error and upgrade build tools

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Upgrade build tools first
+pip install --upgrade pip setuptools wheel
+
+# Install requirements with binary wheels when possible
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
